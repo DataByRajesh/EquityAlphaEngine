00817b397fd90fbdadafe02951d31a97b99fc122
Rajesh kumar Alagesan <rajeshkumaratuk@gmail.com>
2025-08-14 20:50:34 +0100

    Decouple Streamlit screener from data fetch

--- run_uk_data.sh
+++ run_uk_data.sh
@@ -2,4 +2,4 @@
 # Wrapper script to update UK data for the last 10 years
 set -e
 cd "$(dirname "$0")"
-python data_pipeline/UK_data.py --years 10
+python data_pipeline/update_financial_data.py --years 10
