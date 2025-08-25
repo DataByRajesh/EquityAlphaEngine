363340742eb8abf2b3a8efee7b14ef0785fb5445
Rajesh kumar Alagesan <rajeshkumaratuk@gmail.com>
2025-08-25 18:52:38 +0100

    chore: remove trial script with hard-coded API key

--- data_pipeline/trial.py
+++
@@ -1,7 +0,0 @@
-import logging
-import quandl
-
-logger = logging.getLogger(__name__)
-quandl.ApiConfig.api_key = "pXBknNEt9LEV6DRBnfhs"
-data = quandl.get("FRED/CPILFESL")  # US CPI (free dataset)
-logger.info("\n%s", data.tail())
